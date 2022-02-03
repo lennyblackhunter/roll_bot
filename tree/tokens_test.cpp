68978df4f4d5cf646bcf931c436a5716a898d32d
janczarknurek <jacek@beit.tech>
2022-02-03 23:11:14 +0100

    Fix old tokens test
    Signed-off-by: janczarknurek <jacek@beit.tech>

--- tree/tokens_test.cpp
+++ tree/tokens_test.cpp
@@ -6,7 +6,8 @@
 
 int main() {
     std::string s = "1 + 2 *3+(5 - 1)";
-    print_vec(*tokenize(s));
-    print_vec(*tokenize("123-321"));
-    std::cout << tokenize("123abc").has_value() << std::endl;
+    std::vector<char> ops {'+', '*', '-'};
+    print_vec(*tokenize(s, ops));
+    print_vec(*tokenize("123-321", ops));
+    std::cout << tokenize("123abc", ops).has_value() << std::endl;
 }
