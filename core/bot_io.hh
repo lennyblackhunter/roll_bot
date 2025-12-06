7a404147367a24f0cc9079f3fabdc410c000ddfa
Janczar Knurek <janczar.kurek@student.uj.edu.pl>
2025-12-06 22:29:11 +0100

    Abstract bot and basic offline cli

--- core/bot_io.hh
+++ core/bot_io.hh
@@ -26,32 +26,41 @@ enum class BotStatus {
   FAILED = 2
 };
 
+struct CommandHandler {
+  using HandlerType = std::function<bool(std::istringstream &, const User &, BotOutputProtocol &)>;
+  HandlerType handler;
+  std::string help;
+};
+
 class Bot {
  public:
-  using HandlerType = std::function<bool(std::istringstream &, const User &, BotOutputProtocol &)>;
 
   BotStatus handle_msg(std::string_view msg, const User & user) { 
     std::istringstream ss{std::string(msg)};
     std::string command;
     ss >> command;
     auto handler = _handlers.find(command);
-    if (handler != _handlers.end()) {
+    if (handler == _handlers.end()) {
       get_output().write_message(std::format("Command {} not found.", command));
       return BotStatus::COMMAND_NOT_FOUND;
     }
-    if (!_handlers[command](ss, get_output())) {
+    if (handler->second.handler(ss, user, get_output())) {
       return BotStatus::FAILED;
     }
     return BotStatus::OK;
   }
 
-  void register_handler(std::string_view command, HandlerType & h) {
-    _handlers[std::string(command)] = h; //TODO: Better insert plz.
+  void register_handler(std::string_view command, std::string_view help, CommandHandler::HandlerType h) {
+    _handlers[std::string(command)] = CommandHandler{
+      .handler = h,
+      .help = std::string(help)
+    }; //TODO: Better insert plz.
   }
 
-  virtual BotOutputProtocol & get_output();
- private:
-  std::map<std::string, HandlerType> _handlers;
+  virtual BotOutputProtocol & get_output() = 0;
+  virtual ~Bot() = default;
+ protected:
+  std::map<std::string, CommandHandler> _handlers;
 };
 
 
